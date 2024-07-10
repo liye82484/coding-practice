0047644e63db5977341ca8cf7fbed5b7d47323a0
YERIM_e5 <70877858+liye82484@users.noreply.github.com>
2024-07-10 14:02:31 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n % 400 == 0) {
+        cout << 1;
+    }
+    else if (n % 4 == 0 && n % 100 != 0) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+    
+    return 0;
+}
