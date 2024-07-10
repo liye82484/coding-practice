7eaff29ffbfebce9f395ccd8c4a42562f83f5276
YERIM_e5 <70877858+liye82484@users.noreply.github.com>
2024-07-10 14:12:35 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n>89) {
+        cout << "A";
+    }
+    else if (n>79) {
+        cout << "B";
+    }
+    else if(n>69) {
+        cout << "C";
+    }
+    else if(n>59) {
+        cout << "D";
+    }
+    else {
+        cout << "F";
+    }
+    return 0;
+}
